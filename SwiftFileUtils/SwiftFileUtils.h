7cf6904f36918112ad203a8ce848f70bbd112860
Noot <nootfy@gmail.com>
2015-10-04 11:33:38 +1100

    Initial Commit

---
+++ SwiftFileUtils/SwiftFileUtils.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFileUtils.h
+//  SwiftFileUtils
+//
+//  Created by Noot on 4/10/2015.
+//  Copyright © 2015 NF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFileUtils.
+FOUNDATION_EXPORT double SwiftFileUtilsVersionNumber;
+
+//! Project version string for SwiftFileUtils.
+FOUNDATION_EXPORT const unsigned char SwiftFileUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFileUtils/PublicHeader.h>
+
+
